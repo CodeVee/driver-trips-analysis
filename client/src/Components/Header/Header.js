f0d1a96c1802cd8173b4151cc713faf0801efaa8
CodeVee <victorchike247@gmail.com>
2019-12-14 22:29:36 +0100

    removed unused div

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -4,21 +4,19 @@ import './Header.css';
 
 const Header = () => {
   return (
-    <div className="header">
-      <nav className="nav">
-        <ul className="nav-links">
-          <li>
-            <Link to="/">Dashboard</Link>
-          </li>
-          <li>
-            <Link to="/trip">Trip</Link>
-          </li>
-          <li>
-            <Link to="/drivers">Drivers</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <nav className="nav">
+      <ul className="nav-links">
+        <li>
+          <Link to="/">Dashboard</Link>
+        </li>
+        <li>
+          <Link to="/trip">Trip</Link>
+        </li>
+        <li>
+          <Link to="/drivers">Drivers</Link>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
