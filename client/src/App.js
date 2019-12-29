88901b5ed8e9755cd323622b2214e81d440d74fd
CodeVee <victorchike247@gmail.com>
2019-12-29 22:03:40 +0100

    imported driverspage

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './Components/Header/Header';
+import Driverspage from './Pages/Driverspage/Driverspage';
 import './App.css';
 
 import Homepage from './Pages/Homepage/Homepage';
