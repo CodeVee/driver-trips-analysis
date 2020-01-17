ec2fef537bab11ee8f86802da6704f7df01c87ad
CodeVee <victorchike247@gmail.com>
2020-01-17 22:20:23 +0100

    reverted color

--- client/src/Components/Charts/MainChart.js
+++ client/src/Components/Charts/MainChart.js
@@ -10,7 +10,7 @@ const MainChart = ({ values }) => {
       categories: ['Cash', 'Card', 'Total'],
     },
     fill: {
-      colors: ['#094a88'],
+      colors: ['#094a89'],
     },
     dataLabels: {
       enabled: false,
