2b2a8ed853e0f7791380f841e34510fbd4972285
CodeVee <victorchike247@gmail.com>
2020-01-18 23:20:57 +0100

    Reverted Width

--- client/src/Components/Charts/MainChart.js
+++ client/src/Components/Charts/MainChart.js
@@ -49,7 +49,7 @@ const MainChart = ({ values }) => {
             options={options}
             series={series}
             type="bar"
-            width="48"
+            width="480"
           />
           <Chart
             className="mobile-chart"
