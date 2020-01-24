bbcf766a8d1a43e7e5037d5b4a471e61fb57c51b
CodeVee <victorchike247@gmail.com>
2020-01-25 00:20:57 +0100

    declared trips state

--- client/src/Context/Trips/TripsState.js
+++ client/src/Context/Trips/TripsState.js
@@ -2,3 +2,7 @@ import React, { useReducer } from 'react';
 import TripsContext from './tripsContext';
 import TripsReducer from './tripsReducer';
 import { GET_TRIPS, GET_DRIVERS, GET_STATS } from '../types';
+
+const TripsState = prop => {
+  const initialState = {};
+};
