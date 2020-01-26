59bef9dd11185b31f5d83023f4eb523add5684ca
CodeVee <victorchike247@gmail.com>
2020-01-26 23:55:42 +0100

    imported trips state

--- client/src/App.js
+++ client/src/App.js
@@ -5,25 +5,28 @@ import Driverspage from './Pages/Driverspage/Driverspage';
 import Tripspage from './Pages/Tripspage/Tripspage';
 import './App.css';
 import Homepage from './Pages/Homepage/Homepage';
+import TripsState from './Context/Trips/TripsState';
 
 const App = () => {
   return (
     <div className="App">
       <div className="container">
-        <Router>
-          <Header />
-          <Switch>
-            <Route exact path="/" component={Homepage} />
-            <Route exact path="/drivers" component={Driverspage} />
-            <Route exact path="/drivers/:driverID" component={Driverspage} />
-            <Route exact path="/trip" component={Tripspage} />
-            <Route
-              exact
-              path="/trip/:driverName/:tripID"
-              component={Tripspage}
-            />
-          </Switch>
-        </Router>
+        <TripsState>
+          <Router>
+            <Header />
+            <Switch>
+              <Route exact path="/" component={Homepage} />
+              <Route exact path="/drivers" component={Driverspage} />
+              <Route exact path="/drivers/:driverID" component={Driverspage} />
+              <Route exact path="/trip" component={Tripspage} />
+              <Route
+                exact
+                path="/trip/:driverName/:tripID"
+                component={Tripspage}
+              />
+            </Switch>
+          </Router>
+        </TripsState>
       </div>
     </div>
   );
