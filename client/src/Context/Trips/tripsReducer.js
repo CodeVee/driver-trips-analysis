3d98df1a829ceaa44997cadd84a3a54aa007ab14
CodeVee <victorchike247@gmail.com>
2020-01-27 00:01:28 +0100

    implemented get trips on reducer

--- client/src/Context/Trips/tripsReducer.js
+++ client/src/Context/Trips/tripsReducer.js
@@ -2,6 +2,11 @@ import { GET_DRIVERS, GET_TRIPS, GET_STATS } from '../types';
 
 export default (state, action) => {
   switch (action.type) {
+    case GET_TRIPS:
+      return {
+        ...state,
+        trips: action.payload,
+      };
     default:
       return state;
   }
