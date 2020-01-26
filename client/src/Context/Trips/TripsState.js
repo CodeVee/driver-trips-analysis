2972703e02dc50b6caeb5a675ec011ab8f2b5f4b
CodeVee <victorchike247@gmail.com>
2020-01-26 23:47:26 +0100

    Added get trips function

--- client/src/Context/Trips/TripsState.js
+++ client/src/Context/Trips/TripsState.js
@@ -11,4 +11,17 @@ const TripsState = prop => {
   };
 
   const [state, dispatch] = useReducer(TripsReducer, initialState);
+
+  const getTrips = async () => {
+    const trips = await fetch('/api/drivers')
+      .then(data => data.json())
+      .then(data => data.data);
+
+    dispatch({
+      type: GET_TRIPS,
+      payload: trips,
+    });
+  };
+
+  return <TripsContext.Provider />;
 };
