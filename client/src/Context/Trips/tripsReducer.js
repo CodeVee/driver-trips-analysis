75b4b2739e6efb38bea5af3deb8decdc6d4683f9
CodeVee <victorchike247@gmail.com>
2020-01-26 23:58:31 +0100

    Implemented switch

--- client/src/Context/Trips/tripsReducer.js
+++ client/src/Context/Trips/tripsReducer.js
@@ -1,3 +1,8 @@
 import { GET_DRIVERS, GET_TRIPS, GET_STATS } from '../types';
 
-export default (state, action) => {};
+export default (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
