37c62a18996515a6feb7658617f84f64715ce514
CodeVee <victorchike247@gmail.com>
2020-01-29 23:33:13 +0100

    Adjusted trips card

--- client/src/Components/DriverCards/TripsCard.js
+++ client/src/Components/DriverCards/TripsCard.js
@@ -1,13 +1,45 @@
 import React, { Fragment, useContext, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import TripsContext from '../../Context/Trips/tripsContext';
 
 const TripsCard = ({ tableRow }) => {
   const tripsContext = useContext(TripsContext);
-
   useEffect(() => {
     tripsContext.getTrips();
+    tripsContext.getDrivers();
     // eslint-disable-next-line
   }, []);
+
+  const driverNames = tripsContext.drivers.reduce((acc, driver) => {
+    if (!acc[driver.driverID]) {
+      acc[driver.driverID] = { name: driver.name };
+    }
+    return acc;
+  }, {});
+
+  const tableRows = tripsContext.trips.map((trip, index) => {
+    const form = {};
+    try {
+      form.driver = driverNames[trip.driverID].name;
+      form.passenger = trip.user.name;
+      form.amount = trip.billedAmount;
+    } catch {
+      form.driver = 'Unknown Driver';
+      form.passenger = trip.user.name;
+      form.amount = trip.billedAmount;
+    }
+    return (
+      <tr key={index}>
+        <td>{index + 1}</td>
+        <td>{form.driver}</td>
+        <td>{form.passenger}</td>
+        <td>{form.amount}</td>
+        <td>
+          {<Link to={`/trip/${form.driver}/${trip.tripID}`}> Details</Link>}
+        </td>
+      </tr>
+    );
+  });
 
   return (
     <Fragment>
@@ -24,7 +56,7 @@ const TripsCard = ({ tableRow }) => {
             </tr>
           </thead>
           <tbody className="trips-body">
-            {tableRow.map(dataRow => {
+            {tableRows.map(dataRow => {
               return dataRow;
             })}
           </tbody>
